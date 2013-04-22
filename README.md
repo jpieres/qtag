38c3e4a1aac44b727924c9627736679315c3de62
Juan Pieres <jpieres@gmail.com>
2013-04-22 22:56:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# qtag.es: qtag web site Spain
+
+Qtag web application for Spain
+by [Juan Pieres](http://http://es.linkedin.com/in/juanpieres).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qtag::Application.config.secret_token = '718fe7fe7f391fa8de4a99f8877039ced4a559cb40242d9e56341a3de45608163d84b45249cc10279ef2b2e4f8eeaf2ca369880190b2389859f646e55fc19121'
