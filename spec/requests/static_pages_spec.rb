b5de9f399e3d2ac31ab83d46c88e8983d5d741e1
Juan Pieres <jpieres@gmail.com>
2013-04-25 23:20:06 +0200

    Add Twitter Bootstrap and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,41 +2,34 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
-  describe "Home page" do
-    it "should have the h1 'qtag'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'qtag')
-    end
+  subject { page }
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "| Home")
-    end    
+  describe "Home page" do
+    before{ visit root_path }
+
+    it { should have_selector('h1', text: 'qtag') }
+    it { should have_selector('title', text: "| Home") }
   end
 
   describe "Help page" do
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
+    before{ visit help_path }
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "| Help")
-    end    
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: "| Help") }
   end
 
   describe "About page" do
+    before{ visit about_path }
 
-    it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
+    it { should have_selector('h1', text: 'About Us') }
+    it { should have_selector('title', text: "| About Us") }
+  end
 
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "| About Us")
-    end    
+  describe "Contact page" do
+    before{ visit contact_path }
+
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: "| Contact") }
   end
 
 end
