73e4e4482d8b8cf7634a3c8ddeb27d6829d1e648
Juan Pieres <jpieres@gmail.com>
2013-04-28 17:39:28 +0200

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,17 @@ class UsersController < ApplicationController
   
   def new
     @user = User.new
-   @title = "Sign up"
+    @title = "Sign up"
   end
 
   def create
     @user = User.new(params[:user])
     if @user.save
+     sign_in @user
      flash[:success] = "Welcome to Qtag App!"
      redirect_to @user
     else
+      @title = "Sign up"
       render 'new'
     end
   end
